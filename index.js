b9f8d2f90df0ce2946642d0dfc37bc87683309ec
Henry Meehan <henryp.meehan@gmail.com>
2019-10-18 22:19:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 function destructivelyAppendKitten(name){
 kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  kitten.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kitten.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kitten.shift(name)
+}
+
+function appendKitten(name){
+  kitten
+}
