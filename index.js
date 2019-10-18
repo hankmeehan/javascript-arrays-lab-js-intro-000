7dc8e4eb61bc527e4953f11aba75aa2593190723
Henry Meehan <henryp.meehan@gmail.com>
2019-10-18 22:30:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,3 +24,11 @@ function appendKitten(name){
 function prependKitten(name) {
   return [name, ...kittens]
 }
+
+function removeLastKitten(name) {
+  return kittens.slice(0, kittens.slice - 1)
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(1)
+}
